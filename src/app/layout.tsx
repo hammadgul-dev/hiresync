59679d60f6f1f5571c4a88759bfc600a630168bd
Hammad Gul <iamhammad148@gmail.com>
2026-06-06 23:53:44 +0500

    add (main) route group and auth pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type {Metadata} from "next"
 import {Poppins} from "next/font/google"
 import "./globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,11 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={poppins.variable}>
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
