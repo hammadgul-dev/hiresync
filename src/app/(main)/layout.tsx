7bc9989e77295371a201a7ef5b61e741e4290ff7
Hammad Gul <iamhammad148@gmail.com>
2026-06-10 00:00:22 +0500

    add toast notifications using react-hot-toast package

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,14 @@
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import {Toaster} from "react-hot-toast"
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
   return (
     <>
+      <Toaster
+        position="top-center"
+        toastOptions={{duration: 2500, style: {fontSize: "14px"}}}
+      />
       <Navbar />
       {children}
       <Footer />
