64f56205cdfe615b293f25fe97f43d92ba96716c
Hammad Gul <iamhammad148@gmail.com>
2026-06-10 23:39:31 +0500

    add helper function to pass JWT token in requests

---
+++ src/app/utils/apiFetch.ts
@@ -0,0 +1,25 @@
+export default async function apiFetch(
+  apiURL: string,
+  httpMethod = "GET",
+  data?: any,
+) {
+  let token = localStorage.getItem("token")
+  try {
+    let apiResp = await fetch(apiURL, {
+      method: httpMethod,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body:
+        httpMethod === "GET" || httpMethod === "DELETE"
+          ? undefined
+          : JSON.stringify(data),
+    })
+    let apiData = await apiResp.json()
+    if (!apiResp.ok) throw new Error(apiData?.message || "Request Failed!")
+    return apiData
+  } catch (e) {
+    throw new Error((e as Error)?.message || "Failed To Fetch!")
+  }
+}
