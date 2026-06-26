27dc09b23cf90170e9d62eee4831e888f2f6868f
Hammad Gul <iamhammad148@gmail.com>
2026-06-26 23:41:35 +0500

    add post job API route and validation

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,93 @@
+import {NextRequest, NextResponse} from "next/server"
+import {getServerSession} from "next-auth"
+import authOptions from "@/lib/auth"
+import dbConnect from "@/lib/db"
+import jobModel from "@/model/jobModel"
+
+export async function POST(req: NextRequest) {
+  try {
+    let session = await getServerSession(authOptions)
+    if (!session) {
+      return NextResponse.json({message: "Unauthorized"}, {status: 401})
+    }
+    if (session.user.role !== "employer") {
+      return NextResponse.json(
+        {message: "Only Employers can post jobs"},
+        {status: 403},
+      )
+    }
+    await dbConnect()
+    let body = await req.json()
+    let {
+      title,
+      category,
+      jobType,
+      location,
+      salaryMin,
+      salaryMax,
+      currency,
+      experience,
+      deadline,
+      skills,
+      description,
+      requirements,
+      benefits,
+    } = body
+
+    if (
+      !title ||
+      !category ||
+      !jobType ||
+      !location ||
+      !salaryMin ||
+      !salaryMax ||
+      !experience ||
+      !deadline ||
+      !description ||
+      !requirements
+    ) {
+      return NextResponse.json(
+        {message: "All Required fields must be filled"},
+        {status: 400},
+      )
+    }
+
+    if (!skills || skills.length === 0) {
+      return NextResponse.json(
+        {message: "At least one skill required"},
+        {status: 400},
+      )
+    }
+
+    if (Number(salaryMin) >= Number(salaryMax)) {
+      return NextResponse.json(
+        {message: "Max salary must be greater than min"},
+        {status: 400},
+      )
+    }
+
+    let job = await jobModel.create({
+      title,
+      category,
+      jobType,
+      location,
+      salaryMin: Number(salaryMin),
+      salaryMax: Number(salaryMax),
+      currency,
+      experience,
+      deadline: new Date(deadline),
+      skills,
+      description,
+      requirements,
+      benefits,
+      employer: (session.user as any)?.id,
+      companyName: (session.user as any)?.companyName,
+    })
+    return NextResponse.json(
+      {message: "Job posted successfully"},
+      {status: 201},
+    )
+  } catch {
+    return NextResponse.json({message: "Something went wrong"}, {status: 500})
+  }
+}
